23580c2ed27697845e5c81f06828ca6003342938
Nahuel Rudel <nahuelalerudel@gmail.com>
2022-06-16 22:35:46 -0300

    interaccion con html

--- js/main.js
+++ js/main.js
@@ -13,14 +13,38 @@ function generadorAutomatico() {
 }
 generadorAutomatico()
 
-//Agrega productos al carrito
-function generarCarrito() {
-    carrito.push(new Producto(234, "MICRO INTEL I5", 24590))
-    carrito.push(new Producto(567, "PLACA DE VIDEO GTX 1660", 67820))
-    carrito.push(new Producto(890, "RAM 32GB 4800MHZ VIPER", 27800))
-    carrito.push(new Producto(012, "TECLADO CORSAIR K60 PRO", 12820))
+//Cargar el HTML con los productos
+function cargarProductos() {
+    listadoProductos.innerHTML = ""
+    for (const producto of productos) {
+        const li = document.createElement("li") 
+              li.innerText = producto.nombre
+              li.id = producto.id
+              li.addEventListener("click", ()=> { agregarAlCarrito(`${li.innerText}`) } )
+              listadoProductos.append(li)
+    }
 }
-generarCarrito()
+
+cargarProductos()
+
+//Agregar al carrito los productos seleccionados
+function agregarAlCarrito(prod) {
+    if (prod.trim() !== "") {
+        carrito.push(prod)
+        const liNuevoProducto = document.createElement("li")
+              liNuevoProducto.innerText = prod
+              liNuevoProducto.id = prod + "EnCarrito"
+              liNuevoProducto.addEventListener("dblclick", ()=> { removerDelCarrito(`${liNuevoProducto.id}`) }) 
+              listadoCarrito.append(liNuevoProducto)
+    }
+}
+
+//Quitar del carrito los productos seleccionados
+function removerDelCarrito(prod) {
+    const productoAremover = document.getElementById(`${prod}`)
+          productoAremover.remove()
+          console.warn(`${prod} ha sido eliminado del carrito.`)
+}
 
 //Crear ID aleatorio
 function creoID() {
@@ -39,6 +63,7 @@ function agregarProducto() {
     let precio = parseInt(prompt("Ingresa el precio:"))
         productos.push(new Producto(id, nombre, precio))
     alert("Producto agregado correctamente")
+    cargarProductos()
 }
 
 //Listar todos los nombres de los productos incluidos en el array "productos"
@@ -83,7 +108,8 @@ function aumentarPrecios(porc) {
 
 //Sumar todos los precios de los productos incluidos en el carrito
 function calcularCarrito() {
-    console.clear()
+    debugger
+    // console.clear()
     let total = carrito.reduce((acc, producto)=> acc + producto.precio, 0)
         console.log("TOTAL DEL CARRITO:", total)
 }
--- js/variables.js
+++ js/variables.js
@@ -1,3 +1,6 @@
 const IVA = 1.21
 const productos = []
 const carrito = []
+
+const listadoProductos = document.getElementById("listadoProductos")
+const listadoCarrito = document.getElementById("listadoCarrito")
